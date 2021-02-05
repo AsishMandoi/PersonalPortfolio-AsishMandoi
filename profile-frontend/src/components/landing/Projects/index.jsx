7aea9cdf8215c8ac8d8f3342a2b79e2aad6113e6
luckyasish13 <luckyasish13@gmail.com>
2021-02-05 12:46:17 +0530

    Working Website(2)!

--- profile-frontend/src/components/landing/Projects/index.jsx
+++ profile-frontend/src/components/landing/Projects/index.jsx
@@ -66,7 +66,7 @@ export const Projects = () => {
           </Item>
         ))}
       </Grid>
-      <h4><br /><br /><br />More coming soon...</h4>
+      <h4><br /><br />More coming soon...</h4>
     </Wrapper>
   );
 };
